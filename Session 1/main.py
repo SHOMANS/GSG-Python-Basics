c1a98305ee5aa522afdccadad1d1d53ff93d3c2b
SHOMANS <mohammedshoman9@gmail.com>
2026-05-06 19:02:03 +0200

    Session 1

---
+++ Session 1/main.py
@@ -0,0 +1,29 @@
+# this is a simple example
+print('Hello, World!') # this is inline comment
+
+'''
+hello
+hi
+how are you!
+'''
+
+user_name = "Afnan" # => Text => String
+user_family_name = 'Alghamdi' # => Text => String
+user_age = "29" # => string
+is_employed = "False" # => String
+
+age = 24 # => Number => Integer
+
+height = 1.75 # => Number => Float
+user_height = 1.00 # => Number => Float 
+
+is_student = True # => Boolean => True or False => 1 or 0
+
+
+user_name = "Afnan Alghamdi" # => Text => String
+user_name = 24 # => Number => Integer (should describe the user name as a string, but it is now an integer)
+
+x = 5 
+
+
+
